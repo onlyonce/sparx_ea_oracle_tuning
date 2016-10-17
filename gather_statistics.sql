d6a43deb44a453a445e7c80902e66256f30d953b
Tamás Sipos <sipos.tamas@yahoo.com>
2016-10-17 23:41:10 +0200

    Create gather_statistics.sql

---
+++ gather_statistics.sql
@@ -0,0 +1,9 @@
+-- Gathering full statistics for optimizer
+BEGIN
+  dbms_stats.gather_schema_stats(ownname          => 'EA',
+                                 estimate_percent => 100,
+                                 granularity      => 'ALL',
+                                 degree           => 8,
+                                 method_opt       => 'FOR ALL COLUMNS',
+                                 options          => 'GATHER');
+END;
